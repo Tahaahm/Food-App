ebe7a41842d7e00751f609913e800d114b8ae6ef
Taha Ahmad <tahaahmad837@gmail.com>
2023-06-25 17:39:27 +0300

    make it better

--- lib/constants/global_variable.dart
+++ lib/constants/global_variable.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.1.7:3000';
+String uri = 'https://amazon-clone-rrr.herokuapp.com/';
 
 class GlobalVariable {
   static const colorGradient = LinearGradient(colors: [
